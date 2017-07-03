3b8d07d914dce096f9cc937eab89de608e0d1943
Gao, Lei <gldamao@gmail.com>
2017-07-03 20:35:37 +0800

    x

--- src/main/scala-2.11/com/lei/leetcode/P160/Solution.java
+++ src/main/scala-2.11/com/lei/leetcode/P160/Solution.java
@@ -15,7 +15,41 @@ class ListNode {
 }
 
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = len(headA);
+        int lenB = len(headB);
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        while (headA != null && headB != null) {
+            if (headA == headB)
+                return headA;
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
 
+    public static int len(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        ListNode l2 = l1;
+        System.out.println(getIntersectionNode(l1, l2));
     }
 }
