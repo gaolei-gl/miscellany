2c1d3a8ba9f7a8c6c2edef33fd60ad9bc47bf6e7
Gao, Lei <gldamao@gmail.com>
2017-07-15 17:45:28 +0800

    x

---
+++ src/main/scala-2.11/com/lei/leetcode/P532/Solution.java
@@ -0,0 +1,16 @@
+package com.lei.leetcode.P532;
+
+/**
+ * Created by Lei on 2017/7/15.
+ */
+public class Solution {
+    public static int findPairs(int[] nums, int k) {
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{3, 1, 4, 1, 5};
+        System.out.println(findPairs(a, 2));
+    }
+}
