5176192b3dbbb8ad2d2f88e9780c1e79a038e70e
Gao, Lei <gldamao@gmail.com>
2017-07-26 23:57:45 +0800

    x

---
+++ src/main/scala-2.11/com/lei/DI/SimulationEntity.scala
@@ -0,0 +1,21 @@
+package com.lei.DI
+
+/**
+  * Created by Lei on 2017/6/17.
+  */
+trait SimulationEntity {
+  def handleMessage(msg: String, ctx: String): Unit
+}
+
+trait NetworkEntity {
+  def getMacAddress(ip: String): String
+
+  def hasIpAddress(addr: String): Boolean
+
+
+  def handleMessage(msg: String, ctx: String): Unit = msg match {
+    case "1" =>
+    case _ => super.handleMessage(msg, ctx)
+  }
+}
+
