fc0fdb250188ad2b51d30a4931bc1ae1fcdf9d3c
Gao, Lei <gldamao@gmail.com>
2017-09-08 10:05:14 +0800

    x

---
+++ src/main/java/com/lei/reflect/InstanceByReflection.java
@@ -0,0 +1,14 @@
+package com.lei.reflect;
+
+import org.reflections.Reflections;
+
+public class InstanceByReflection {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class clazz = Class.forName("com.lei.reflect.RealObject");
+        Reflections reflections = new Reflections("com.lei");
+        for (Class s  : reflections.getSubTypesOf(com.lei.reflect.MyInterface.class))
+            System.out.println(s);
+//        MyInterface iface = (MyInterface) clazz.newInstance();
+//        iface.doSomething();
+    }
+}
