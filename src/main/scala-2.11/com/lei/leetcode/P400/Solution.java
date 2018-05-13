cb71c7af960ff604eb7d8bed77575d6c44d7c98a
Gao, Lei <gldamao@gmail.com>
2018-05-13 23:23:57 +0800

    400. Nth Digit

---
+++ src/main/scala-2.11/com/lei/leetcode/P400/Solution.java
@@ -0,0 +1,27 @@
+package com.lei.leetcode.P400;
+
+/**
+ * https://leetcode.com/problems/nth-digit/description/
+ */
+public class Solution {
+    public int findNthDigit(int n) {
+        int len = 1;
+        long count = 9;
+        int start = 1;
+
+        while (n > len * count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n - 1) / len;
+        String s = Integer.toString(start);
+        return Character.getNumericValue(s.charAt((n - 1) % len));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
