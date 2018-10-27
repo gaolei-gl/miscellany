62f20af274a4fa2a1e079b734eed97e6d65f4660
Gao, Lei <gldamao@gmail.com>
2018-10-27 10:56:07 +0800

    449. Serialize and Deserialize BST(Done)

--- src/main/scala-2.11/com/lei/leetcode/P449/Codec.java
+++ src/main/scala-2.11/com/lei/leetcode/P449/Codec.java
@@ -12,27 +12,25 @@ public class Codec {
     public String serialize(TreeNode root) {
         if (root == null) return "*";
         else {
-            return root.val + serialize(root.left) + serialize(root.right);
+            return String.valueOf(root.val) + ',' + serialize(root.left) + ',' + serialize(root.right);
         }
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        List<Integer> chs = new ArrayList<>(data.length());
-        for (char c : data.toCharArray()) {
-            if (c - '0' >= 0)
-                chs.add(c - '0');
-            else chs.add('*' - 0);
+        List<String> chs = new ArrayList<>();
+        for (String s : data.split(",")) {
+            chs.add(s);
         }
         return deserialize(chs);
     }
 
-    private TreeNode deserialize(List<Integer> data) {
+    private TreeNode deserialize(List<String> data) {
         if (data.isEmpty()) return null;
-        int c = data.remove(0);
+        String s = data.remove(0);
         // means this is null
-        if (c == '*') return null;
-        TreeNode node = new TreeNode(c);
+        if (s.equals("*")) return null;
+        TreeNode node = new TreeNode(Integer.parseInt(s));
         node.left = deserialize(data);
         node.right = deserialize(data);
         return node;
@@ -42,8 +40,8 @@ public class Codec {
     public static void main(String[] args) {
         Codec codec = new Codec();
         TreeNode tree = new TreeNode(2);
-        tree.left = new TreeNode(1);
-        tree.right = new TreeNode(3);
+        tree.right = new TreeNode(1);
+        tree.right.right = new TreeNode(3);
         System.out.println(codec.serialize(tree));
         System.out.println(codec.serialize(codec.deserialize(codec.serialize(tree))));
     }
