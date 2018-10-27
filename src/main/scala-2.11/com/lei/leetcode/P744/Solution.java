f76c5a9815907f3154fdfd1184633e9ac4dcca7a
Gao, Lei <gldamao@gmail.com>
2018-10-27 18:28:26 +0800

    reformat

--- src/main/scala-2.11/com/lei/leetcode/P744/Solution.java
+++ src/main/scala-2.11/com/lei/leetcode/P744/Solution.java
@@ -3,8 +3,9 @@ package com.lei.leetcode.P744;
 import java.util.Arrays;
 
 /**
- * Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
- * <p>
+ * Given a list of sorted characters letters containing only lowercase letters,
+ * and given a target letter target, find the smallest element in the list that is larger than
+ * the given target.
  * Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
  */
 public class Solution {
