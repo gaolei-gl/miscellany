a59520cf5b045bcf4263b7e6d2eb04cb3ecaf7f2
Gao, Lei <gldamao@gmail.com>
2018-11-28 21:21:00 +0800

    238. Product of Array Except Self

---
+++ src/main/scala-2.11/com/lei/leetcode/P238/Solution.java
@@ -0,0 +1,17 @@
+package com.lei.leetcode.P238;
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = nums[i - 1] * res[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
