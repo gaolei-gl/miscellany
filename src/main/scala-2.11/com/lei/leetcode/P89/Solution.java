8e35339e4e8b339aec2a6876b10149995e6a4213
Gao, Lei <gldamao@gmail.com>
2018-12-03 23:07:13 +0800

    89. Gray Code

---
+++ src/main/scala-2.11/com/lei/leetcode/P89/Solution.java
@@ -0,0 +1,12 @@
+package com.lei.leetcode.P89;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new LinkedList<>();
+        for (int i = 0; i < 1<<n; i++) result.add(i ^ i>>1);
+        return result;
+    }
+}
