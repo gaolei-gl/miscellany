fddca2e9dec3c221004a93719565f21b972cc264
Gao, Lei <gldamao@gmail.com>
2018-12-03 08:49:32 +0800

    15. 3Sum

---
+++ src/main/scala-2.11/com/lei/leetcode/P15/Solution.java
@@ -0,0 +1,29 @@
+package com.lei.leetcode.P15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int sum = 0 - nums[i];
+                int lo = i + 1, hi = nums.length - 1;
+                while (lo < hi) {
+                    if (nums[lo] + nums[hi] == sum) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    } else if (nums[lo] + nums[hi] > sum) hi--;
+                    else lo++;
+                }
+            }
+        }
+        return res;
+    }
+}
