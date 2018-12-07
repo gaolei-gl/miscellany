1d554726bacc46ae5330cd4e770efa02dfb0d3e4
Gao, Lei <gldamao@gmail.com>
2018-12-07 13:15:19 +0800

    762. Prime Number of Set Bits in Binary Representation

---
+++ src/main/scala-2.11/com/lei/leetcode/P762/Solution.java
@@ -0,0 +1,17 @@
+package com.lei.leetcode.P762;
+
+class Solution {
+    public int countPrimeSetBits(int L, int R) {
+        int ans = 0;
+        for (int x = L; x <= R; ++x)
+            if (isSmallPrime(Integer.bitCount(x)))
+                ans++;
+        return ans;
+    }
+
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
+    }
+}
+
