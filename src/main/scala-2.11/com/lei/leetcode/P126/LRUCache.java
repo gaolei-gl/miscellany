0d90534c601b6ad9683b9eaa644dfc4ce80fa74a
Gao, Lei <gldamao@gmail.com>
2018-12-07 15:59:56 +0800

    146. LRU Cache

---
+++ src/main/scala-2.11/com/lei/leetcode/P126/LRUCache.java
@@ -0,0 +1,106 @@
+package com.lei.leetcode.P126;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Design and implement a data structure for Least Recently Used (LRU) cache.
+ * It should support the following operations: get and put.
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * put(key, value) - Set or insert the value if the key is not already present.
+ * When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ * Follow up:
+ * Could you do both operations in O(1) time complexity?
+ */
+public class LRUCache {
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            this.key = k;
+            this.value = v;
+        }
+
+        Node() {
+            this(0, 0);
+        }
+    }
+
+    private int capacity, count;
+    private Map<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.count = 0;
+        map = new HashMap<>();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        Node n = map.get(key);
+        if (null == n) {
+            return -1;
+        }
+        update(n);
+        return n.value;
+    }
+
+    public void put(int key, int value) {
+        Node n = map.get(key);
+        if (null == n) {
+            n = new Node(key, value);
+            map.put(key, n);
+            add(n);
+            ++count;
+        } else {
+            n.value = value;
+            update(n);
+        }
+        if (count > capacity) {
+            Node toDel = tail.prev;
+            remove(toDel);
+            map.remove(toDel.key);
+            --count;
+        }
+    }
+
+    private void update(Node node) {
+        remove(node);
+        add(node);
+    }
+
+    private void add(Node node) {
+        Node after = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = after;
+        after.prev = node;
+    }
+
+    private void remove(Node node) {
+        Node before = node.prev, after = node.next;
+        before.next = after;
+        after.prev = before;
+    }
+
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2 /* capacity */);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        cache.get(2);       // returns -1 (not found)
+        cache.put(4, 4);    // evicts key 1
+        cache.get(1);       // returns -1 (not found)
+        cache.get(3);       // returns 3
+        cache.get(4);       // returns 4
+
+    }
+}
