8bda0ee90cec96022391bfacd7e29bc62f0917d1
Gao, Lei <gldamao@gmail.com>
2018-12-14 23:00:21 +0800

    M 94. Binary Tree Inorder Traversal

---
+++ src/main/scala-2.11/com/lei/leetcode/P94/Solution.java
@@ -0,0 +1,25 @@
+package com.lei.leetcode.P94;
+
+import com.lei.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}
