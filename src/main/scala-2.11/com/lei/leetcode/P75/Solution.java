94bd9df7d8cdeac29e12c48d30d933ebee2c2635
Gao, Lei <gldamao@gmail.com>
2018-12-19 14:00:15 +0800

    M 75. Sort Colors

---
+++ src/main/scala-2.11/com/lei/leetcode/P75/Solution.java
@@ -0,0 +1,20 @@
+package com.lei.leetcode.P75;
+
+public class Solution {
+    void sortColors(int[] nums) {
+        int n = nums.length;
+        int n0 = -1, n1 = -1, n2 = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                nums[++n2] = 2;
+                nums[++n1] = 1;
+                nums[++n0] = 0;
+            } else if (nums[i] == 1) {
+                nums[++n2] = 2;
+                nums[++n1] = 1;
+            } else if (nums[i] == 2) {
+                nums[++n2] = 2;
+            }
+        }
+    }
+}
