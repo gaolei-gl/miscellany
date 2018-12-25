55937dbc3f3f5f833ef2a8e5efdf09ca3513119e
Gao, Lei <gldamao@gmail.com>
2018-12-25 17:29:44 +0800

    E 852. Peak Index in a Mountain Array

---
+++ src/main/scala-2.11/com/lei/leetcode/P852/Solution.java
@@ -0,0 +1,9 @@
+package com.lei.leetcode.P852;
+
+public class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int i = 0;
+        while (A[i] < A[i+1]) i++;
+        return i;
+    }
+}
