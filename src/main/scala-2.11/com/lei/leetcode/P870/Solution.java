4ffb6c3db0055527009be2f007775caf7b43e60e
Gao, Lei <gldamao@gmail.com>
2018-12-26 17:00:23 +0800

    M 870. Advantage Shuffle

---
+++ src/main/scala-2.11/com/lei/leetcode/P870/Solution.java
@@ -0,0 +1,29 @@
+package com.lei.leetcode.P870;
+
+import java.util.*;
+
+public class Solution {
+    public int[] advantageCount(int[] A, int[] B) {
+        List<Integer> AA = new ArrayList<>(A.length);
+        for (int i1 : A) AA.add(i1);
+        Collections.sort(AA);
+        int[] res = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            int idx = Collections.binarySearch(AA, B[i] + 1);      // see comment
+            if (idx < 0) idx = -idx - 1;
+            if (idx < AA.size()) res[i] = AA.remove(idx);
+            else res[i] = AA.remove(0);
+        }
+        return res;
+    }
+}
+
+/*
+[8,2,4,4,5,6,6,0,4,7]
+[0,8,7,4,4,2,8,5,2,0]
+this is a edge case, there are same value occur multiple times. and if we use binary search to find B[i] in AA, it
+will produce equal values and not utilize the max "respect", so we find the current value +1 to make sure "bigger" not equal.
+
+ */
+
+
