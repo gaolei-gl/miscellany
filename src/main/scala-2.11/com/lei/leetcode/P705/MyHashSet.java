c430e4a52454c212530379d5c1d0f294d352f8f8
Gao, Lei <gldamao@gmail.com>
2018-12-26 12:27:28 +0800

    E 705. Design HashSet

---
+++ src/main/scala-2.11/com/lei/leetcode/P705/MyHashSet.java
@@ -0,0 +1,47 @@
+package com.lei.leetcode.P705;
+
+public class MyHashSet {
+
+    private int buckets = 1000;
+    private int itemsPerBucket = 1001;
+    private boolean[][] table;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashSet() {
+        table = new boolean[buckets][];
+    }
+
+    public int hash(int key) {
+        return key % buckets;
+    }
+
+    public int pos(int key) {
+        return key / buckets;
+    }
+
+    public void add(int key) {
+        int hashkey = hash(key);
+
+        if (table[hashkey] == null) {
+            table[hashkey] = new boolean[itemsPerBucket];
+        }
+        table[hashkey][pos(key)] = true;
+    }
+
+    public void remove(int key) {
+        int hashkey = hash(key);
+
+        if (table[hashkey] != null)
+            table[hashkey][pos(key)] = false;
+    }
+
+    /**
+     * Returns true if this set did not already contain the specified element
+     */
+    public boolean contains(int key) {
+        int hashkey = hash(key);
+        return table[hashkey] != null && table[hashkey][pos(key)];
+    }
+}
