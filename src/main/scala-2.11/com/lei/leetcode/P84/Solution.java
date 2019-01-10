919a9dd509a24aad863117ca82f9cc6f06c8dc44
Gao, Lei <gldamao@gmail.com>
2019-01-10 09:40:04 +0800

    WIP

---
+++ src/main/scala-2.11/com/lei/leetcode/P84/Solution.java
@@ -0,0 +1,39 @@
+package com.lei.leetcode.P84;
+
+import java.util.Stack;
+
+public class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < heights.length; i++) {
+            if (stack.isEmpty() || heights[stack.peek()] <= heights[i]) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+                    int pre = stack.pop();
+                    max = Math.max(max, heights[pre] * (stack.isEmpty() ? pre + 1 : i - pre));
+                }
+                stack.push(i);
+            }
+        }
+        while (!stack.isEmpty()) {
+            int pre = stack.pop();
+            max = Math.max(max, heights[pre] * (stack.isEmpty() ? heights.length : heights.length - 1 - stack.peek()));
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.largestRectangleArea(new int[]{4, 2, 0, 3, 2, 5}));
+        /*
+        [1,2,3,4,5,3,3,2]
+[2,1,2]
+[2,1,5,6,2,3]
+[5,4,1,2]
+[4,2,0,3,2,5]
+[3,6,5,7,4,8,1,0]
+         */
+    }
+}
