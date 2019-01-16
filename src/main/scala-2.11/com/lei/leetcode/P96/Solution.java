78c4531932357dca3211edeef2346c3b37b0f1e3
Gao, Lei <gldamao@gmail.com>
2019-01-16 08:11:06 +0800

    M 96. Unique Binary Search Trees

---
+++ src/main/scala-2.11/com/lei/leetcode/P96/Solution.java
@@ -0,0 +1,21 @@
+package com.lei.leetcode.P96;
+
+public class Solution {
+    public int numTrees(int n) {
+        if (n < 2) return 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.numTrees(3));
+    }
+}
