e6a66b740ce441abfc6d202545d9375a6b24b423
Gao, Lei <gldamao@gmail.com>
2019-01-18 21:17:22 +0800

    E 100. Same Tree

---
+++ src/main/scala-2.11/com/lei/leetcode/P100/Solution.java
@@ -0,0 +1,13 @@
+package com.lei.leetcode.P100;
+
+import com.lei.leetcode.TreeNode;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
