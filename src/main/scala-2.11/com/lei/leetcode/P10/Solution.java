4c0663094b1f5e757715ef4dafc7a7aae248c87e
Gao, Lei <gldamao@gmail.com>
2019-01-25 06:18:01 +0800

    H 10. Regular Expression Matching

---
+++ src/main/scala-2.11/com/lei/leetcode/P10/Solution.java
@@ -0,0 +1,21 @@
+package com.lei.leetcode.P10;
+
+public class Solution {
+    public boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isMatch("aaa", ".*"));
+    }
+}
